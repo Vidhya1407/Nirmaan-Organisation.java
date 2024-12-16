57a08bd6e016fcd4b96e99ca4009aea64a100f03
Vidhya1407 <132552299+Vidhya1407@users.noreply.github.com>
2024-12-16 16:57:53 +0530

    Day 1- task 2

---
+++ VariableTask.java
@@ -0,0 +1,24 @@
+public class VariableTask{
+public static void main(String a[])
+{
+byte b=1;
+short s=12;
+int i=10;
+long l=9854765;
+float f=3.14f;
+double d=3.142857d;
+char c='A';
+boolean B=true;
+String str="Vidhya S";
+
+System.out.println("byte = "+b);
+System.out.println("short = "+s);
+System.out.println("int = "+i);
+System.out.println("long = "+l);
+System.out.println("float = "+f);
+System.out.println("double = "+d);
+System.out.println("char = "+c);
+System.out.println("boolean = "+B);
+System.out.println("String = "+str);
+}
+}
