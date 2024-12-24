788a8471c778d4ae417277e4947b1e47a436face
Vidhya1407 <132552299+Vidhya1407@users.noreply.github.com>
2024-12-24 15:54:36 +0530

    Day 4

---
+++ loops.java
@@ -0,0 +1,75 @@
+package day4;
+
+import java.util.Scanner;
+
+public class loops {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+/*// PRINT TABLE :
+ 	System.out.println("Enter your table : ");
+ 
+	int n=sc.nextInt();
+	for(int i=1; i<=n;i++) {
+		System.out.println(n+"*"+i+"="+n*i);
+		if (i>=10)
+			break;
+		}
+		
+//***********************************************************	
+	for(int i=0; i<=10;i++) {
+		System.out.println(i);
+		if (i%5==0)
+			System.out.println("Vidhya");
+		}
+		
+//***********************************************************
+	for(int i=1; i<=50;i++) {
+		
+		if (i==25)
+			break;
+		else
+			System.out.println(i);
+		}
+//***********************************************************
+for(int i=1; i<=50;i++) {
+		
+		if (i==25)
+			continue;
+		else
+			System.out.println(i);
+		}
+//***********************************************************
+	for(int i=1; i<=10;i++) {
+		for(int j=1; j<=5;j++) {
+			System.out.print(j);
+		}
+		
+	}
+//**********************************************************
+	for(int i=1; i<=10;i++) {
+		for(int j=1; j<=5;j++) {
+			System.out.println(i);
+		}
+//**********************************************************
+		for(int i=1; i<=5;i++) {
+			for(int j=1; j<=5;j++) {
+				System.out.print(i);
+			}
+			System.out.println();
+	}
+//**********************************************************
+			for(int i=1; i<=5;i++) {
+				for(int j=1; j<=5;j++) {
+					System.out.print(j);
+				}
+				System.out.println();
+		}*/
+//**********************************************************
+	for(int i=1; i<=5;i++) {
+		for(int j=1; j<=5;j++) {
+			System.out.print("* ");
+		}
+		System.out.println();
+}
+}
+}
