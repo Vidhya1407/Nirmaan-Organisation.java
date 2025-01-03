e2532dfe5ec80caa413e3a65c021d0ab166a3174
Vidhya1407 <132552299+Vidhya1407@users.noreply.github.com>
2025-01-03 15:04:25 +0530

    Day 9

---
+++ Constructor.java
@@ -0,0 +1,12 @@
+package day9;
+
+public class Constructor {
+	public static void main(String[] args) {
+		Calculator cal=new Calculator();
+		System.out.println(cal.add(20, 5));
+		System.out.println(cal.sub(20, 5));
+		System.out.println(cal.mul(20, 5));
+		System.out.println(cal.div(20, 5));
+	}
+
+}
